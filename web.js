c0aee1e42e7250df679510adbd4b469b34ea5505
Ubuntu <ubuntu@ip-172-31-7-150.ap-northeast-1.compute.internal>
2013-07-20 02:16:30 +0000

    web.js changed

--- web.js
+++ web.js
@@ -6,11 +6,14 @@ var fs = require('fs');
 
 var buffer = new Buffer(fs.readFileSync("index.html"));
 
+var htmlfile = "index.html";
+
 app.get('/', function(request, response) {
-  response.send(buffer.toString())
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
